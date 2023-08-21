d096aa88bfb6ffdf8784fd1d650461cbfd01f761
Sylvia T <sylviarosemarie@gmail.com>
2023-08-21 10:32:52 -0500

    Updated placed order module to add the prices of the user's selections and display them in the placed order box on the page

--- scripts/placedOrders.js
+++ scripts/placedOrders.js
@@ -1,12 +1,15 @@
 export const showOrders = async () => {
-  const fetchResponse = await fetch("http://localhost:8088/orders");
+  const fetchResponse = await fetch(
+    "http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size"
+  );
   const orders = await fetchResponse.json();
-
   let ordersHTML = `<section>`;
 
   ordersHTML += orders
     .map((order) => {
-      return `<div>Order #${order.id}</div>`;
+      const orderPrice =
+        order.metal.price + order.style.price + order.size.price;
+      return `<div>Order #${order.id} costs $${orderPrice} </div>`;
     })
     .join("");
 
